10dde3cb6c15edeb3970a4cd0101ad3d4187673f
Szymon <szymonex@vp.pl>
2021-03-18 10:25:16 +0100

    Initial Commit

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition; //what point of world this node represent
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+}
